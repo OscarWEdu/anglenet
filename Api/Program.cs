7a13cb2f906218ce4240a29c851b4ad9bbf2521b
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-17 22:32:02 +0200

    Set up EntityFramework.
    Set up for localdb, used to more easily migrate to Azure in production.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,12 @@
+using anglenet;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddCors(options =>
 {
