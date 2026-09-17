ed672db077e26cf923867e98106441fe1bf5a303
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-18 00:21:01 +0200

    Quick structure refactoring of the Api endpoints

--- Api/Program.cs
+++ Api/Program.cs
@@ -30,52 +30,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowAngularDev");
 
-app.MapGet("/api/tests", async (AppDbContext db) =>
-{
-    return await db.Tests.ToListAsync();
-});
-
-app.MapPost("/api/tests", async (Test test, AppDbContext db) =>
-{
-    db.Tests.Add(test);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/api/tests/{test.Id}", test);
-});
-
-app.MapPost("/api/auth/register", async (
-    RegisterRequest request,
-    AppDbContext db,
-    IPasswordHasher<User> passwordHasher) =>
-    {
-        if (string.IsNullOrWhiteSpace(request.Username) || string.IsNullOrWhiteSpace(request.Password))
-        {
-            return Results.BadRequest("Missing Field.");
-        }
-
-        if (await db.Users.AnyAsync(u => u.Username == request.Username))
-        {
-            return Results.Conflict("User already exists.");
-        }
-
-        var user = new User
-        {
-            Username = request.Username
-        };
-
-        user.PwHash = passwordHasher.HashPassword(
-            user,
-            request.Password);
-
-        db.Users.Add(user);
-        await db.SaveChangesAsync();
-
-        return Results.Created($"/api/users/{user.Id}", new
-        {
-            user.Id,
-            user.Username
-        });
-    }
-);
+ApiEndpoints.MapEndpoints(app);
 
 app.Run();
