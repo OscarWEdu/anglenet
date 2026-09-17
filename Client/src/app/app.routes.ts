1618da131baca4a35adf858c485389e606860eed
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-17 15:34:17 +0200

    Add routing via Angular

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Quotes } from './pages/quotes/quotes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: Home },
+	{ path: 'login', component: Login },
+	{ path: 'quotes', component: Quotes }
+];
