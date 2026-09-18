055a5ee32b807f766e4b41f7ce8ac92c403293b6
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-18 03:24:45 +0200

    Set up Profile page

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Quotes } from './pages/quotes/quotes';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: Home },
 	{ path: 'login', component: Login },
 	{ path: 'register', component: Login },
+	{ path: 'profile', component: Profile },
 	{ path: 'quotes', component: Quotes }
 ];
