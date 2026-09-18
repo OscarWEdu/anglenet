1237cf4947bacebdf3a660fd806d04585687a13e
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-09-18 03:30:52 +0200

    Add a canActivate guard helper method for profile page navigation

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Quotes } from './pages/quotes/quotes';
 import { Profile } from './pages/profile/profile';
+import { authGuard } from './services/auth.guard';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: Home },
 	{ path: 'login', component: Login },
 	{ path: 'register', component: Login },
-	{ path: 'profile', component: Profile },
+	{ path: 'profile', component: Profile, canActivate: [authGuard] },
 	{ path: 'quotes', component: Quotes }
 ];
